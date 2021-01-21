640540e0e0702652d65b6a310bc72ea73b80bd73
Sheikh Rashed <mdrashed1310@gmail.com>
2021-01-21 20:35:45 +0600

    Updated ui

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 // Var 
-
-
 let Author = document.querySelectorAll('.author');
+// Author name
 Author.forEach( au => au.textContent= "Sheikh Rashed");
+
+
+
+// UI -var
+const bill = document.getElementById('bill');
+
+
+
+// console.log(billAmount,serviceQuality,totalTip,totalBill);
+
+// Add event listeners for calculate
